9239caa6e065510b6d8f5b410f8c74450782c8c6
aschootemeijer <abelschootemeijer@gmail.com>
2024-12-11 14:38:34 +0100

    Update montecarlo_sim.py

--- montecarlo_sim.py
+++ montecarlo_sim.py
@@ -13,18 +13,18 @@ start = time.time()
 save_fig = False
 
 # ASSIGN SOME VALUES THAT WE WILL USE LATER
-whose_data = 's23' #f03, k22, s23  # foellmi+2003, KaranDsilva+2022, Schootemeijer+2024
-s_id       = 9  # 1 --> SMC AB1 etc
-mwr     = 20  # assumed mass of the Wolf-Rayet star, in Solar units
-pdist   = 'powerlaw' # Vasco, Langer, Renzo, or powerlaw. Papers: Langer+2020, Renzo+2019
-logp_exponent = 0. # if we choose powerlaw.  #1.44 is pi_max lmc, -0.18 is pi_max smc based on obs periods
+whose_data = 's23'            	#f03, k22, s23  # foellmi+2003, KaranDsilva+2022, Schootemeijer+2024
+s_id       = 9  		# 1 --> SMC AB1 etc
+mwr     = 20  			# assumed mass of the Wolf-Rayet star, in Solar units
+pdist   = 'powerlaw' 		# Vasco, Langer, Renzo, or powerlaw. Papers: Langer+2020, Renzo+2019
+logp_exponent = 0. 		# if we choose powerlaw.  #1.44 is pi_max lmc, -0.18 is pi_max smc based on obs periods
 logpmin, logpmax = 0.00001, 3.5
 
-eccdist = 'exponential'#'exponential'   # circular, exponential     # which eccentricity distribution?
-ecc_exp_factor= 0    # if we choose exponential
-qmin, qmax = 0.05, 2 # min. and max. mass ratio of the hypothetical binary components
-n_universes   = 11 # 1 universe mean: simulate once per P_orb+q combination
-sigma_line_var  = 0      # non_binary variability. Enter 0 if we do not want to bother. units: km/s
+eccdist = 'exponential'		#'exponential'   # circular, exponential     # which eccentricity distribution?
+ecc_exp_factor= 0    		# if we choose exponential
+qmin, qmax = 0.05, 2 		# min. and max. mass ratio of the hypothetical binary components
+n_universes   = 11 		# 1 universe mean: simulate once per P_orb+q combination
+sigma_line_var  = 0      	# non_binary variability. Enter 0 if we do not want to bother. units: km/s
 set_err_to_zero = True
 
 # let save name of figure depend on assumptions about eccentricity etc
@@ -43,10 +43,10 @@ def draw_orbital_conf(fbin):
         logp = np.random.choice(list(logps), p=list(logp_probs))
         p_in_d  = 10**logp
         rat     = qmin + (qmax-qmin)*b       
-        poffset = c*1.e7                 # random period offset
+        poffset = c*1.e7    	# random period offset
         # eccentricy, if it is taken into account
         if not eccdist == 'circular':
-            omega   = d*2*np.pi               # random orientation of this longitude of ascending note or whatever we call this
+            omega   = d*2*np.pi # random orientation of this longitude of ascending note or whatever we call this
             ecc     = np.random.choice( eccs, p=ecc_probs )
     return binary, p_in_d, rat, poffset, omega, ecc
 
@@ -58,21 +58,21 @@ for inc in incs:
 incprobs = incprobs/sum(incprobs)
 # make lists with logps 
 logps_pl, logp_probs_pl     = np.linspace(logpmin, logpmax,335), []
-for logp_pl in logps_pl:  #pl as in power law, one that isnt broken
+for logp_pl in logps_pl:  	#pl as in power law, one that isnt broken
     logp_probs_pl.append( logp_pl**logp_exponent )
 logp_probs_pl = logp_probs_pl/sum(logp_probs_pl)
 # make lists with eccentricities
 eccs, ecc_probs     = np.linspace(0, 0.9, 451), []
-for ecc in eccs:  #pl as in power law, one that isnt broken
+for ecc in eccs:  		#pl as in power law, one that isnt broken
     ecc_probs.append( np.exp( ecc_exp_factor*ecc ) )
 ecc_probs = ecc_probs/sum(ecc_probs)
 
 # here we define a extra orbital period distributions that can be also be used
 # broken power law Porb distribution
 if pdist == 'Vasco':
-    x1, x2, x3 = 0., 1.25, 3.5   # x2 is peak, others are where y=0. Say peak is at y=1 (we norm. ltr)
+    x1, x2, x3 = 0., 1.25, 3.5	# x2 is peak, others are where y=0. Say peak is at y=1 (we norm. ltr)
 if pdist == 'Langer':
-    x1, x2, x3 = 0.5, 2.25, 3    # x2 is peak, others are where y=0. Say peak is at y=1 (we norm. ltr)
+    x1, x2, x3 = 0.5, 2.25, 3 	# x2 is peak, others are where y=0. Say peak is at y=1 (we norm. ltr)
 if pdist == 'Vasco' or pdist == 'Langer':
     y1, y2, y3 = 0., 1., 0.
     slope12 = (y2-y1)/(x2-x1)
@@ -94,7 +94,7 @@ if pdist == 'Vasco' or pdist == 'Langer':
 # with the function below we can get the radial velocity semi-amplitude of a certain binary
 grav_const, msol = 6.67e-11, 1.99e30 #si
 def get_k( p_in_d, rat, inc, ecc ):   # eq 2.92 in M. Benaquista book
-    porb = p_in_d * 86400  # go to porb in s
+    porb = p_in_d * 86400  	# go to porb in s
     m2 = mwr * rat
     mtot = mwr + m2
     kwr = ( (m2*msol)**3/(mtot*msol)**2 * (grav_const*2*np.pi)/(porb) )**(1./3) * 0.001
@@ -121,12 +121,12 @@ def func(y, val):
     x = ( y-ecc*sin(y) ) / (2.*np.pi) - val
     return x
 def get_RV( K,omega,gamma,ecc, phase ):
-    E_min_sinE  = phase                # phase is equivalent to random E_min_sinE norm. to 1 but I fear I might forget 
+    E_min_sinE  = phase       	# phase is equivalent to random E_min_sinE norm. to 1 but I fear I might forget 
     lala        = fsolve( func,0.5,E_min_sinE )
     E           = lala[0]
     if E <= np.pi:
         theta = np.arccos(  (np.cos(E)-ecc) / (1 - ecc*np.cos(E))  )
-    if E > np.pi:   # arccos returns only values between 0 and pi, theta goes to 2+pi in real life
+    if E > np.pi:  		 # arccos returns only values between 0 and pi, theta goes to 2+pi in real life
         theta = 2*np.pi - np.arccos(  (np.cos(E)-ecc) / (1 - ecc*np.cos(E))  )  # tested that it is symmetric
     RV = K * ( np.cos(theta+omega)+ecc*np.cos(omega) ) + gamma
     return RV
@@ -137,7 +137,7 @@ ax=plt.subplot(111 )
 # here we make lists of lists because we call the lists later when we loop over smc and lmc (although usually we only enter 1 s_id etc)
 s_ids = [ s_id ]
 m_ids = [1,2,3,6,7,10,110,127,128,133,138,139,141,151,152,157]
-m_ids = [1] # karan+22 fig 5 is only WR1
+m_ids = [1] 			# karan+22 fig 5 is only WR1
 ids_list = [s_ids]
 
 rv_file_m = np.empty(( n_universes,len(m_ids) ))
@@ -156,7 +156,7 @@ if whose_data == 's23':
     fldr = 'data_ourobs'
 
 # define the P_orb and mass ratio parameter space that we explore
-nlogps, nrats = 36,40     # Dsilva22: 51,39
+nlogps, nrats = 36,40     	# Dsilva22: 51,39
 logps_sim = np.linspace( logpmin,logpmax,nlogps )
 rats_sim  = np.linspace( qmin,qmax,nrats )
 X,Y       = np.meshgrid( logps_sim,rats_sim*20 )
@@ -172,7 +172,7 @@ rv_file = rv_files[0]
 ids = ids_list[0]
 deltarv_obs = []
 if whose_data == 's23': rvcol = 'rv'
-for i,id_i in enumerate(ids):  # indent also not necessary anymore because we stopped running the script for multiple sources
+for i,id_i in enumerate(ids): 
     if whose_data == 'f03': s = ascii.read( '%s/ab%s.csv'%(fldr,id_i) )# ab for f03; AB for s23
     if whose_data == 's23': s = ascii.read( '%s/AB%s.csv'%(fldr,id_i) )# ab for f03; AB for s23
     tobs  = s[tcol]
@@ -180,17 +180,17 @@ for i,id_i in enumerate(ids):  # indent also not necessary anymore because we st
     vrad_errs = s[rverrcol]
 	print('ID %s'%id_i)
     delta_rv_tresh = max(vrads) - min(vrads)
-    for x,logp in enumerate(logps_sim):     # get period
+    for x,logp in enumerate(logps_sim): 			# get period
         p_in_d = 10**logp
         delta_rv_list_rat_integrated = []
-        for y,rat in enumerate(rats_sim):   # get mass ratio
+        for y,rat in enumerate(rats_sim):   			# get mass ratio
             delta_rv_list = []
-            for j in range( n_universes):   # for n_universes times, we draw a binary star constellation
+            for j in range( n_universes):   			# for n_universes times, we draw a binary star constellation
                 binary_notused,p_in_d_notused,rat_notused,poffset, omega, ecc = draw_orbital_conf(1) # we don't use binary because we assume a binary fraction of 1
-                inc = np.random.choice(incs, p=incprobs)    # draw inclination based on lists that we made
-                kwr = get_k(p_in_d, rat, inc, ecc)          # get radial velocity amplitude
+                inc = np.random.choice(incs, p=incprobs)   	# draw inclination based on lists that we made
+                kwr = get_k(p_in_d, rat, inc, ecc)          	# get radial velocity amplitude
                 klist = []
-                for k in range( len( tobs ) ):              # make a list of radial velocity measurements in the synthetic binary
+                for k in range( len( tobs ) ):			# make a list of radial velocity measurements in the synthetic binary
                     if set_err_to_zero == False:
                         rand_err = np.random.normal(0, vrad_errs[k] )
                     else: rand_err = 0
@@ -201,7 +201,7 @@ for i,id_i in enumerate(ids):  # indent also not necessary anymore because we st
                     if ecc == 0:
                         rv_k  = kwr * np.cos( 2*np.pi*phase )
                     elif ecc > 0:
-                        rv_k  = get_RV( kwr,omega,0,ecc, phase )   # 0 is gamma, we don't care about v_sys
+                        rv_k  = get_RV( kwr,omega,0,ecc, phase ) # 0 is gamma, we don't care about v_sys
                     klist.append( rv_k + rand_err +line_var_err)
                 delta_rv = max(klist) - min(klist)
                 delta_rv_list.append(delta_rv)
@@ -267,8 +267,8 @@ for i, row in enumerate(Z):
 		print( i, rats_sim[i], j, logps_sim[j], row[j])
 		pprob_j ,qprob_i = pprobs_l20[j], qprobs_l20[i]
 		prob_total_l20 += pprob_j*qprob_i
-		prob_l20 += row[j] * pprob_j*qprob_i   # row[j] is p_observe
-		if logps_sim[j] < 1.5: pprob_j = 0    # EXPERIMENT: take them out if the distribution because they have been detected
+		prob_l20 += row[j] * pprob_j*qprob_i  		# row[j] is p_observe
+		if logps_sim[j] < 1.5: pprob_j = 0    		# EXPERIMENT: take them out if the distribution because they have been detected
 		prob_total_l20_30d_plus += pprob_j*qprob_i
 		prob_l20_30d_plus       += row[j] * pprob_j*qprob_i   # row[j] is p_observe
 print( "\nLanger+20 logPdist qdist integrated prob: %s"%(prob_l20/prob_total_l20) )
@@ -284,8 +284,8 @@ for i, row in enumerate(Z):
 		pprob_j ,qprob_i = pprobs_r19[j], 0
 		if rats_sim[i] >= 1.5: qprob_i = 1
 		prob_total_r19 += pprob_j*qprob_i
-		prob_r19       += row[j] * pprob_j*qprob_i  # row[j] is p_observe
-		if logps_sim[j] < 1.5: pprob_j = 0    # EXPERIMENT: take them out if the distribution because they have been detected
+		prob_r19       += row[j] * pprob_j*qprob_i  	# row[j] is p_observe
+		if logps_sim[j] < 1.5: pprob_j = 0    		# EXPERIMENT: take them out if the distribution because they have been detected
 		prob_total_r19_30d_plus += pprob_j*qprob_i
 		prob_r19_30d_plus       += row[j] * pprob_j*qprob_i  # row[j] is p_observe
 print( "Renzo+19 logPdist qdist integrated prob: %s"%(prob_r19/prob_total_r19) )
@@ -295,7 +295,7 @@ print( "Renzo+19 logPdist 30d+ qdist integrated prob: %s"%(prob_r19_30d_plus/pro
 prob, prob_1yr, prob_qmin, prob_1yr_qmin, prob_30dplus= 0, 0, 0, 0, 0
 for i, row in enumerate(Z):
 	prob += sum(row)
-	if rats_sim[i] > 0.145*2:  # initial mass ration was twice as large; Mini = 40, Mcurr = 20. So we proceed if M>5Msol or M>=6Msol, however you call it
+	if rats_sim[i] > 0.145*2:  				# initial mass ration was twice as large; Mini = 40, Mcurr = 20. So we proceed if M>5Msol or M>=6Msol, however you call it
 		prob_qmin += sum(row)
 	for j in range(len(row)):
 		if logps_sim[j] < np.log10(365.25):
@@ -305,10 +305,10 @@ for i, row in enumerate(Z):
 		if logps_sim[j] > np.log10(30):
 			prob_30dplus += row[j]
 print('prob/tot', prob/(nlogps*nrats) )
-print('prob_qmin/tot', prob_qmin/(nlogps*(nrats-5) ))  # last 10 logP bins are larger than 1yr
-print('prob_1yrmax/tot', prob_1yr/((nlogps-10)*nrats) )   # last 10 logP bins are larger than 1yr
+print('prob_qmin/tot', prob_qmin/(nlogps*(nrats-5) ))  		# last 10 logP bins are larger than 1yr
+print('prob_1yrmax/tot', prob_1yr/((nlogps-10)*nrats) )   	# last 10 logP bins are larger than 1yr
 print('prob_1yrmax_qmin/tot', prob_1yr_qmin/((nlogps-10)*(nrats-5)) )   # last 10 logP bins are larger than 1yr
-print('prob_30dplus/tot', prob_30dplus/((nlogps-15)*(nrats)) )   # last 10 logP bins are larger than 1yr
+print('prob_30dplus/tot', prob_30dplus/((nlogps-15)*(nrats)) ) 	# last 10 logP bins are larger than 1yr
 
 # draw contours..
 ax.contour( X,Y,Z,[0.05, 0.5, 0.95],linewidth=0.4, colors=['k'] )#,cmap='autumn' )
